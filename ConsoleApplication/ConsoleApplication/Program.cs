72b9450b9332bbaef0cb6341868446fdde778f51
splucena <sumilangplucena@yahoo.com.ph>
2020-09-17 21:22:55 +0800

    Added first console appliation

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, country;
+
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your country: ");
+            country = Console.ReadLine();
+            Console.WriteLine("Hello my name is " + name + ", I am from the " + country);
+        }
+    }
+}
--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -6,7 +6,21 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * ( width + height ) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is" + woodLength + " feet");
+            Console.WriteLine("The area of the glass is " + glassArea + " square meters");
         }
     }
 }
