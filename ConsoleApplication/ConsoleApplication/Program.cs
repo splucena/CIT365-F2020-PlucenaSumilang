e35aa9b3abe03bb303ea5273aec4a5b46f59b29c
splucena <sumilangplucena@yahoo.com.ph>
2020-09-17 23:04:36 +0800

    updated consoleapplication project

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -13,7 +13,37 @@ namespace ConsoleApplication
 
             Console.Write("Enter your country: ");
             country = Console.ReadLine();
-            Console.WriteLine("Hello my name is " + name + ", I am from the " + country);
+
+            Console.WriteLine("My name is {0}, I am from {1}",name, country);
+
+            string date;
+            date = DateTime.UtcNow.ToString("MM-dd-yyyy");
+            Console.WriteLine("The current date is {0}", date);
+
+            DateTime dateChristmas, currentDate;
+            currentDate = DateTime.Now;
+            dateChristmas = Convert.ToDateTime("12/25/2020");
+
+            Console.WriteLine("{0} Days before Christmas!", (dateChristmas - currentDate).Days);
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.Write("Enter width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.Write("Enter height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is {0} feet", woodLength);
+            Console.WriteLine("The area of the glass is {0} square meters", glassArea);
+
+            Console.ReadKey();
         }
     }
 }
