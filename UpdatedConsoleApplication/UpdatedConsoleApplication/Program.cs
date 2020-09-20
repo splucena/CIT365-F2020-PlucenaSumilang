beaaac02d5114b46e9840560d7c86e4433aba45d
splucena <sumilangplucena@yahoo.com.ph>
2020-09-21 00:20:00 +0800

    Updated console application to use correct framework

---
+++ UpdatedConsoleApplication/UpdatedConsoleApplication/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpdatedConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, country;
+
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your country: ");
+            country = Console.ReadLine();
+
+            // String interpolation
+            Console.WriteLine($"My name is {name}, I am from {country}\n");
+
+            string date;
+            date = DateTime.UtcNow.ToString("MM-dd-yyyy");
+            // String concatenation using '+' sign
+            Console.WriteLine("The current date is " + date.ToString() + " \n");
+
+            DateTime dateChristmas, currentDate;
+            currentDate = DateTime.Now;
+            dateChristmas = Convert.ToDateTime("12/25/2020");
+
+            // Composite formatting
+            Console.WriteLine("{0} Days before Christmas!\n", (dateChristmas - currentDate).Days);
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            Console.Write("Enter width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.Write("Enter height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            // String.Concat()
+            Console.WriteLine(String.Concat("The length of the wood ", woodLength, " is feet"));
+            Console.WriteLine(String.Concat("The area of the glass is ", glassArea, " square meters"));
+
+            Console.ReadKey();
+        }
+    }
+}
