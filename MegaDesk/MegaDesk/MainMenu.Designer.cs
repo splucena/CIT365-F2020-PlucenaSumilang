259d857b680eb6571f2ddaeb1a29df9080f2eefc
splucena <sumilangplucena@yahoo.com.ph>
2020-09-28 03:07:26 +0800

    added MegaDesk project.

---
+++ MegaDesk/MegaDesk/MainMenu.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace MegaDesk
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddNewQuote = new System.Windows.Forms.Button();
+            this.btnViewQuotes = new System.Windows.Forms.Button();
+            this.btnSearchQuotes = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddNewQuote
+            // 
+            this.btnAddNewQuote.AutoSize = true;
+            this.btnAddNewQuote.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(86)))), ((int)(((byte)(161)))));
+            this.btnAddNewQuote.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddNewQuote.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNewQuote.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(203)))), ((int)(((byte)(227)))));
+            this.btnAddNewQuote.Location = new System.Drawing.Point(29, 31);
+            this.btnAddNewQuote.Name = "btnAddNewQuote";
+            this.btnAddNewQuote.Padding = new System.Windows.Forms.Padding(10);
+            this.btnAddNewQuote.Size = new System.Drawing.Size(209, 52);
+            this.btnAddNewQuote.TabIndex = 0;
+            this.btnAddNewQuote.Text = "&Add New Quote";
+            this.btnAddNewQuote.UseVisualStyleBackColor = false;
+            this.btnAddNewQuote.Click += new System.EventHandler(this.btnAddNewQuote_Click);
+            // 
+            // btnViewQuotes
+            // 
+            this.btnViewQuotes.AutoSize = true;
+            this.btnViewQuotes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(86)))), ((int)(((byte)(161)))));
+            this.btnViewQuotes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewQuotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewQuotes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(203)))), ((int)(((byte)(227)))));
+            this.btnViewQuotes.Location = new System.Drawing.Point(29, 89);
+            this.btnViewQuotes.Name = "btnViewQuotes";
+            this.btnViewQuotes.Padding = new System.Windows.Forms.Padding(10);
+            this.btnViewQuotes.Size = new System.Drawing.Size(209, 52);
+            this.btnViewQuotes.TabIndex = 1;
+            this.btnViewQuotes.Text = "&View Quotes";
+            this.btnViewQuotes.UseVisualStyleBackColor = false;
+            this.btnViewQuotes.Click += new System.EventHandler(this.btnViewQuotes_Click);
+            // 
+            // btnSearchQuotes
+            // 
+            this.btnSearchQuotes.AutoSize = true;
+            this.btnSearchQuotes.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(86)))), ((int)(((byte)(161)))));
+            this.btnSearchQuotes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearchQuotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearchQuotes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(203)))), ((int)(((byte)(227)))));
+            this.btnSearchQuotes.Location = new System.Drawing.Point(29, 147);
+            this.btnSearchQuotes.Name = "btnSearchQuotes";
+            this.btnSearchQuotes.Padding = new System.Windows.Forms.Padding(10);
+            this.btnSearchQuotes.Size = new System.Drawing.Size(209, 52);
+            this.btnSearchQuotes.TabIndex = 2;
+            this.btnSearchQuotes.Text = "&Search Quotes";
+            this.btnSearchQuotes.UseVisualStyleBackColor = false;
+            this.btnSearchQuotes.Click += new System.EventHandler(this.btnSearchQuotes_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AutoSize = true;
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(86)))), ((int)(((byte)(161)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(203)))), ((int)(((byte)(227)))));
+            this.btnExit.Location = new System.Drawing.Point(29, 205);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Padding = new System.Windows.Forms.Padding(10);
+            this.btnExit.Size = new System.Drawing.Size(209, 52);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MegaDesk.Properties.Resources.workplace;
+            this.pictureBox1.Location = new System.Drawing.Point(265, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 226);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(197)))), ((int)(((byte)(203)))), ((int)(((byte)(227)))));
+            this.ClientSize = new System.Drawing.Size(511, 288);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSearchQuotes);
+            this.Controls.Add(this.btnViewQuotes);
+            this.Controls.Add(this.btnAddNewQuote);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MegaDesk";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddNewQuote;
+        private System.Windows.Forms.Button btnViewQuotes;
+        private System.Windows.Forms.Button btnSearchQuotes;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ MegaDesk/MegaDesk/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddNewQuote_Click(object sender, EventArgs e)
+        {
+            //AddQuote.ActiveForm.Show();
+            AddQuote a = new AddQuote();
+            a.ShowDialog();
+        }
+
+        private void btnViewQuotes_Click(object sender, EventArgs e)
+        {
+            ViewAllQuotes v = new ViewAllQuotes();
+            v.ShowDialog();
+        }
+
+        private void btnSearchQuotes_Click(object sender, EventArgs e)
+        {
+            SearchQuotes s = new SearchQuotes();
+            s.ShowDialog();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result == DialogResult.OK) 
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
