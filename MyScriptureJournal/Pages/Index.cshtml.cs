050097c307e10a6e14a8cf52e9714e26b6f28a36
splucena <sumilangplucena@yahoo.com.ph>
2020-10-27 21:25:26 +0800

    added sorting

--- MyScriptureJournal/Pages/Index.cshtml.cs
+++ MyScriptureJournal/Pages/Index.cshtml.cs
@@ -23,20 +23,46 @@ namespace MyScriptureJournal.Pages
 
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
+        public string BookSort { get; set; }
+        public string DateAddedSort { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            ScriptureJournal = await _context.ScriptureJournal.ToListAsync();
-
+            //ScriptureJournal = await _context.ScriptureJournal.ToListAsync();
+            // Search by book or keyword from note field
             var journals = from sj in _context.ScriptureJournal
                            select sj;
 
             if (!string.IsNullOrEmpty(SearchString))
             {
                 journals = journals.Where(b => b.Book.Contains(SearchString) || b.Note.Contains(SearchString));
-            }
 
-            ScriptureJournal = await journals.ToListAsync();
+                ScriptureJournal = await journals.ToListAsync();
+            }
+            else
+            {
+                // Implement sorting
+                BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+                DateAddedSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+                IQueryable<ScriptureJournal> journalsIQ = from sj in _context.ScriptureJournal
+                                                          select sj;
+
+                switch (sortOrder)
+                {
+                    case "book_desc":
+                        journalsIQ = journalsIQ.OrderByDescending(sj => sj.Book);
+                        break;
+                    case "date_added_desc":
+                        journalsIQ = journalsIQ.OrderBy(sj => sj.DateAdded);
+                        break;
+                    default:
+                        journalsIQ = journalsIQ.OrderBy(sj => sj.ID);
+                        break;
+                }
+
+                ScriptureJournal = await journalsIQ.AsNoTracking().ToListAsync();
+            }
         }
     }
 }
