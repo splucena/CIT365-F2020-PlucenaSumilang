0e363dc5c57dce1ecaf7ca3a80e9861b1f68d520
splucena <sumilangplucena@yahoo.com.ph>
2020-11-22 14:31:27 +0800

    update design.

--- ContosoUniversity/Models/Department.cs
+++ ContosoUniversity/Models/Department.cs
@@ -21,6 +21,7 @@ namespace ContosoUniversity.Models
         [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
 
+        [Display(Name = "Instructor")]
         public int? InstructorID { get; set; }
 
         [Timestamp]
