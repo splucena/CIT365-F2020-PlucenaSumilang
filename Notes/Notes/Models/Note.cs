55142bd505d20a41989b90eab7441de9fba4f82e
splucena <sumilangplucena@yahoo.com.ph>
2020-12-05 16:58:56 +0800

    add database

--- Notes/Notes/Models/Note.cs
+++ Notes/Notes/Models/Note.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using SQLite;
 
 namespace Notes.Models
 {
-    class Note
+    public class Note
     {
-        public string Filename { get; set; }
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
         public string Text { get; set; }
         public DateTime Date { get; set; }
     }
